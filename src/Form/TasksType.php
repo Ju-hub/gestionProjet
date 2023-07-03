fe1958c5c7e0c9febb194ebbdb9f00f477138989
Julien <julien_cristovao@hotmail.com>
2023-07-03 12:40:24 +0200

    Choice type priority et status ✅

--- src/Form/TasksType.php
+++ src/Form/TasksType.php
@@ -7,6 +7,7 @@ use App\Entity\Tasks;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -20,16 +21,26 @@ class TasksType extends AbstractType
         $builder
             ->add('name', TextType::class)
             ->add('description', TextareaType::class)
-            ->add('priority', TextType::class)
-            ->add('status', TextType::class)
+            ->add('priority', ChoiceType::class, [
+                "choices" => [
+                    "Normale" => "P_NORMALE",
+                    "Urgente" => "P_URGENTE"
+                ]
+            ])
+            ->add('status', ChoiceType::class, [
+                "choices" => [
+                    "A faire" => "S_A_FAIRE",
+                    "En cours" => "S_EN_COURS",
+                    "Réalisée" => "S_DONE"
+                ]
+                ])
             ->add('date_end', DateTimeType::class)
             // ->add('projects', EntityType::class, [
             //     'class' => Projects::class])
             ->add('userTask', EntityType::class, [
                 'class' => User::class,
                 'multiple' => true,
-                'expanded' => true,
-                'mapped' => true    // Mapped true afin d'attribuer un ou plusieurs User à une tache           
+                'expanded' => true  // Mapped true afin d'attribuer un ou plusieurs User à une tache           
                 ])
             ->add('Ajouter', SubmitType::class)
         ;
